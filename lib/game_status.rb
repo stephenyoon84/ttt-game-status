17c9b0d913c981f750e5edff6b708c1253e9d348
stephenyoon84 <stephenyoon84@gmail.com>
2019-02-10 20:53:11 -0500

    add game status

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,64 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+]
+
+def won?(board)
+  is_won = false
+  for win_combination in WIN_COMBINATIONS
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      return win_combination
+    else
+      false
+    end
+  end
+  is_won
+end
+
+def full?(board)
+  is_full = true
+  for i in 0..8
+    if !position_taken?(board, i)
+      is_full = false
+    end
+  end
+  return is_full
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if draw?(board)
+    return true
+  elsif won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  else
+    return nil
+  end
+
+end
